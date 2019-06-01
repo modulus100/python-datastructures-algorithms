06bd415df15cb5951eb4997e83ca5f72cf267670
modulu100 <aleksandr.madisson@gmail.com>
2019-06-01 18:08:57 +0300

    traverse

---
+++ binary_tree.py
@@ -0,0 +1,46 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def get_left_child(self):
+        return self.left
+
+    def set_left_child(self, node):
+        self.left = node
+
+    def get_right_child(self):
+        return self.right
+
+    def set_right_child(self, node):
+        self.right = node
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+class Tree(object):
+    def __init__(self, value):
+        if isinstance(value, Node):
+            self.root = value
+        else:
+            self.root = Node(value)
+
+    def get_root(self):
+        return self.root
