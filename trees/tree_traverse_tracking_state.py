2c1073ae241c0cf6f01176d9c836cfcf3e51752a
modulus100 <aleksandr.madisson@gmail.com>
2019-06-02 15:48:06 +0300

    separate files

--- trees/tree_traverse_tracking_state.py
+++ trees/tree_traverse_tracking_state.py
@@ -1,64 +1,7 @@
 from trees.Tree import Tree
 from trees.Node import Node
-
-
-class Stack():
-    def __init__(self):
-        self.list = list()
-
-    def push(self, value):
-        self.list.append(value)
-
-    def pop(self):
-        return self.list.pop()
-
-    def top(self):
-        if len(self.list) > 0:
-            return self.list[-1]
-        else:
-            return None
-
-    def is_empty(self):
-        return len(self.list) == 0
-
-    def __repr__(self):
-        if len(self.list) > 0:
-            s = "<top of stack>\n_________________\n"
-            s += "\n_________________\n".join([str(item) for item in self.list[::-1]])
-            s += "\n_________________\n<bottom of stack>"
-            return s
-
-        else:
-            return "<stack is empty>"
-
-
-class State(object):
-    def __init__(self, node):
-        self.node = node
-        self.visited_left = False
-        self.visited_right = False
-
-    def get_node(self):
-        return self.node
-
-    def get_visited_left(self):
-        return self.visited_left
-
-    def get_visited_right(self):
-        return self.visited_right
-
-    def set_visited_left(self):
-        self.visited_left = True
-
-    def set_visited_right(self):
-        self.visited_right = True
-
-    def __repr__(self):
-        s = f"""{self.node}
-            visited_left: {self.visited_left}
-            visited_right: {self.visited_right}
-        """
-        return s
+from trees.Stack import Stack
+from trees.State import State
 
 
 # create a tree and add some nodes
