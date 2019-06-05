2f62c69ad5169dbbd763909fe1d62979cf080168
modulus100 <aleksandr.tserepannikov@tptlive.ee>
2019-06-05 08:30:42 +0300

    tree updated

--- trees/Tree.py
+++ trees/Tree.py
@@ -1,12 +1,80 @@
 from trees.Node import Node
+from trees.Queue import Queue
 
 
-class Tree(object):
+class Tree():
     def __init__(self, value):
         if isinstance(value, Node):
             self.root = value
         else:
             self.root = Node(value)
 
+    def set_root(self, value):
+        self.root = Node(value)
+
     def get_root(self):
         return self.root
+
+    def compare(self, node, new_node):
+        """
+        0 means new_node equals node
+        -1 means new node less than existing node
+        1 means new node greater than existing node
+        """
+        if new_node.get_value() == node.get_value():
+            return 0
+        elif new_node.get_value() < node.get_value():
+            return -1
+        else:
+            return 1
+
+    """
+    define insert here
+    can use a for loop (try one or both ways)
+    """
+
+    def insert_with_loop(self, new_value):
+        pass
+
+    """
+    define insert here (can use recursion)
+    try one or both ways
+    """
+
+    def insert_with_recursion(self, value):
+        pass
+
+    def __repr__(self):
+        level = 0
+        q = Queue()
+        visit_order = list()
+        node = self.get_root()
+        q.enq((node, level))
+
+        while (len(q) > 0):
+            node, level = q.deq()
+            if node == None:
+                visit_order.append(("<empty>", level))
+                continue
+            visit_order.append((node, level))
+            if node.has_left_child():
+                q.enq((node.get_left_child(), level + 1))
+            else:
+                q.enq((None, level + 1))
+
+            if node.has_right_child():
+                q.enq((node.get_right_child(), level + 1))
+            else:
+                q.enq((None, level + 1))
+
+        s = "Tree\n"
+        previous_level = -1
+        for i in range(len(visit_order)):
+            node, level = visit_order[i]
+            if level == previous_level:
+                s += " | " + str(node)
+            else:
+                s += "\n" + str(node)
+                previous_level = level
+
+        return s
---
+++ trees/traverse/binary_search_tree.py
@@ -0,0 +1,4 @@
+from trees.Node import Node
+from trees.Tree import Tree
+from trees.Queue import Queue
+
